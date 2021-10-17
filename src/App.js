f84af3e9e5dc7e9b20502b5f2f1fe20d797faefa
cohojin3714@gmail.com <kimhojin3714@naver.com>
2021-10-17 23:27:17 +0900

    learn : JSX , Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
 
+const image1 = new Image(); 
+image1.src= 'img/kimchi.jpeg';
+const image2 = new Image(); 
+image2.src= 'public/img/soju.jpeg';
+const image3 = new Image(); 
+image3.src= './img/sam';
+const image4 = new Image(); 
+image4.src= './img/beer';
+
+const Food = ({name , picture}) =>{
+  return (
+    <div>
+      <h3>I Like {name}</h3>
+      <img src= {picture} alt=""/>
+    </div>
+  );
+};
+
+const foodILike = [
+  { 
+    id:1,
+    name: "kimchi", 
+    image: image1
+  },
+  {
+    id:2,
+    name: "soju", 
+    image: image2
+  },
+  {
+    id:3,
+    name: "sam", 
+    image: image3
+  },
+  {
+    id:4,
+    name: "beer", 
+    image: image4
+  },
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(food => <Food key={food.id} name={food.name} picture={food.image}/>)}
     </div>
   );
 }
