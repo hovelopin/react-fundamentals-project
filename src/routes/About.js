067d5bef3ea29f5a42e962c5a02d649893d02b50
cohojin3714@gmail.com <kimhojin3714@naver.com>
2021-10-21 23:28:51 +0900

    learn : react-router-dom

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function About() {
+  console.log("화면이 출력되었습니다.");
+  return (
+    <span>About this page: I built it because I love movies</span>
+  );
+}
+
+export default About;
